5a64620598f99df5d30674739b91e3ac659ae0e8
ungacy <ungacy@126.com>
2019-06-11 14:34:32 +0800

    version bump to 0.1.51

--- SIUtils/Classes/InputValidator/SIInputValidator.m
+++ SIUtils/Classes/InputValidator/SIInputValidator.m
@@ -28,7 +28,7 @@ static NSDictionary *_SIInputValidatorRegexMap;
         @(SIInputValidatorTypePhoneNum): @"^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$",
         @(SIInputValidatorTypeEmail): @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\\.)+[a-zA-Z]{2,}))$",
         @(SIInputValidatorTypeID): @"^(\\d{14}|\\d{17})(\\d|[xX])$",
-        @(SIInputValidatorTypePassword): @"((?=.*[a-zA-Z])(?=.*[0-9])|(?=.*[0-9])(?=.*[@#$%&/=?_.,:;\\-])|(?=.*[a-zA-Z])(?=.*[@#$%&/=?_.,:;\\-])|(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%&/=?_.,:;\\-])).{6,32}",
+        @(SIInputValidatorTypePassword): @"((?=.*[a-zA-Z])(?=.*[0-9])|(?=.*[0-9])(?=.*[@#$%&/=?_.,:;\\-])|(?=.*[a-zA-Z])(?=.*[@#$%&/=?_.,:;\\-])|(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%&/=?_.,:;\\-])).{8,32}",
         @(SIInputValidatorTypePassport): @"^1[45][0-9]{7}|([P|p|S|s]\\d{7})|([S|s|G|g]\\d{8})|([Gg|Tt|Ss|Ll|Qq|Dd|Aa|Ff]\\d{8})|([H|h|M|m]\\d{8，10})$",
         @(SIInputValidatorTypeAllianceName): @"[\u4e00-\u9fa5a-zA-Z0-9]{2,15}",
         @(SIInputValidatorTypeAllianceCode): @"[A-Z]{2,15}",
